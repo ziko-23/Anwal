92cc32ded04d29e738ce904712b6039e494d60d7
ziko-23 <kabouchziko3@gmail.com>
2022-07-30 17:07:43 +0100

    le 15

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -43,7 +43,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("august 05, 2022 21:00:00").getTime();
+    this.deadline = new Date("august 15, 2022 21:00:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
